f2136a562d5ade80e4c41b0d860974ba0334df24
linroex <linroex@coder.tw>
2015-02-17 07:35:48 +0800

    init write README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+## WordPress Media Folder
+
+適用於 WordPress 的外掛，可以讓你的 WordPress 網站有媒體櫃的功能，做到「文件下載」的前台頁面。
---
+++ media_folder.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WordPress Media Folder
+ * Plugin URI: https://github.com/linroex/WordPress-Media-Folder
+ * Description: 可以顯示你最近上傳的檔案、上傳時間，做出媒體櫃的概念，並指定要顯示的檔案格式
+ * Version: 0.0.1
+ * Author: linroex
+ * Author URI: http://coder.tw
+ * License: GPL2
+ */
